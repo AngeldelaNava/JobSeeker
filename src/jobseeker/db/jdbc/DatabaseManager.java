7ed4deda8b28a3e5c89cdb3c51a5bf7f928dd689
Angel de la Nava <a.nava1@usp.ceu.es>
2021-02-19 19:07:16 +0100

    First commit

---
+++ src/jobseeker/db/jdbc/DatabaseManager.java
@@ -0,0 +1,22 @@
+package jobseeker.db.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jobseeker.db.Job;
+import jobseeker.db.Person;
+
+public class DatabaseManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Job garbageColector = new Job();
+		Person manolo = new Person("Manolo", null);
+		garbageColector.addPerson(manolo);
+		List<Job> jobs = new ArrayList<Job>();
+		jobs.add(garbageColector);
+		manolo.setJobs(jobs);
+		System.out.println(manolo);
+	}
+
+}

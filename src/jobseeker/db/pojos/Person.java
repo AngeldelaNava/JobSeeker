072de7fd185c4f854836ff618d98285695e0628c
Angel de la Nava <a.nava1@usp.ceu.es>
2021-03-01 17:55:55 +0100

    id added

--- src/jobseeker/db/pojos/Person.java
+++ src/jobseeker/db/pojos/Person.java
@@ -9,6 +9,7 @@ public class Person implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -1077803972062837560L;
+	private Integer id;
 	private String name;
 	private List<Job> jobs;
 	
@@ -18,6 +19,20 @@ public class Person implements Serializable {
 		this.jobs = jobs;
 	}
 	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
 	public String getName() {
 		return name;
 	}
@@ -31,14 +46,19 @@ public class Person implements Serializable {
 		this.jobs = jobs;
 	}
 
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((jobs == null) ? 0 : jobs.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
+
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -49,6 +69,11 @@ public class Person implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (jobs == null) {
 			if (other.jobs != null)
 				return false;
@@ -62,10 +87,14 @@ public class Person implements Serializable {
 		return true;
 	}
 
+
+
 	@Override
 	public String toString() {
-		return "Person [name=" + name + ", jobs=" + jobs + "]";
+		return "Person [id=" + id + ", name=" + name + ", jobs=" + jobs + "]";
 	}
+
+	
 	
 	
 	
